83c6a3f13c123eee758eb71b0d0938356a0b7989
Noah Portes Chaikin <noah@porteschaikin.com>
2019-07-29 14:01:40 -0400

    Finish first cut of action

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,52 @@
 import * as core from "@actions/core"
+import axios, { AxiosResponse } from "axios"
+import { readFileSync } from "fs"
+import snake from "snakecase-keys"
+import camel from "camelcase-keys"
+
+interface Deploy {
+  revision: string
+  branch: string
+  environment: string | null
+  version: string | null
+  repositoryUrl: string
+}
+
+const DEPLOYS_URL: string =
+  process.env.VELOCITY_DEPLOYS_URL || "https://velocity.codeclimate.com/deploys"
+
+const GITHUB_EVENT_PATH: string = process.env.GITHUB_EVENT_PATH as string
+const readEvent = (): any =>
+  camel(JSON.parse(readFileSync(GITHUB_EVENT_PATH, "utf8")))
+
+const report = (deploy: Deploy): Promise<AxiosResponse<any>> =>
+  axios.post(
+    DEPLOYS_URL,
+    snake({
+      branch: deploy.branch,
+      environment: deploy.environment,
+      revision: deploy.revision,
+      version: deploy.version,
+      repositoryUrl: "foo",
+      token: process.env.VELOCITY_DEPLOYMENT_TOKEN,
+    })
+  )
 
 const main = (): void => {
-  console.log("Hello world")
+  try {
+    const event = readEvent()
+
+    report({
+      revision: process.env.GITHUB_SHA as string,
+      branch: process.env.GITHUB_REF as string,
+      environment:
+        core.getInput("environment") || (event.deployment || {}).environment,
+      version: core.getInput("version"),
+      repositoryUrl: event.repository.webUrl,
+    })
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
 
 main()
